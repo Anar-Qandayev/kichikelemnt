1845150a0916a854ec6614a8c4c129afa0f87b13
Anar-Qandayev <tu7bxfga8@code.edu.az>
2022-03-05 12:35:16 +0400

    first commit

---
+++ arraykichikelement/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace arraykichikelement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region
+            int[] arr = { 5, 3, 9, 23, 35, 55 };
+            int min = arr[0];//5
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine(min);
+            #endregion
+            #region
+            int count = 0;
+            string ad = "anar fdggv asasds atr45";
+            for (int i = 0; i < ad.Length; i++)
+            {
+                if (ad[i] == 'a' || ad[i] == 'A')
+                {
+                    count++;
+
+                }
+            }
+            Console.WriteLine(count + " A herfi var");
+            #endregion
+        }
+
+    }
